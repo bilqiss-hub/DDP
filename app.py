e9f43d52616a6c0e58e733994bac8087f136b0db
bilqiss-hub <grijjen1822@gmail.com>
2025-12-11 14:29:48 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+st.title("Form Data Diri")
+st.write("Silahkan isi data diri anda")
+st.write("Made by intan")
+
+with st.form("form_data_diri"):
+    nama = st.text_input("Nama")
+    alamat = st.text_input("Alamat")
+    usia = st.text_input("Usia")
+    submit = st.form_submit_button("submit")
+    
+    
+if submit:
+    st.success(f"Terima kasih{nama} telah mengisi form data diri")
+    st.write(f"Nama : {nama}")
+    st.write(f"Alamat : {alamat}")
+    st.write(f"Usia : {usia}") 
